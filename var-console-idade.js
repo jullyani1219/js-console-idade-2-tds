71571b05c25e97edcfc324e1e52be3b0be4e1c2d
jullyani1219 <160244521+jullyani1219@users.noreply.github.com>
2024-11-25 12:23:09 -0300

    carga_js-console-idade

---
+++ var-console-idade.js
@@ -0,0 +1,12 @@
+const idademinima = 18;
+const idadecliente = 16;
+
+if (idadecliente >= idademinima ) {
+    console.log ("cerveja") 
+} else { 
+    console.log ("suco")
+}
+     
+  // condição  // true  // false
+  console.log(idadecliente >= idademinima? "cerveja" : "suco") 
+    
